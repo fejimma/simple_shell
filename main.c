95d3dc5a12cd97f1d1af398e4d31ea90ed0f5e3c
Irene Agbani <ireneagbani64@gmail.com>
2023-06-26 03:43:35 -0700

    correction

--- main.c
+++ main.c
@@ -1,21 +1,45 @@
-#include <stdio.h>
+#include "shell.h"
 
 /**
- * print_hello - Prints "Hello, world!"
- */
-void print_hello(void)
-{
-	printf("Hello, world!\n");
-}
-
-/**
- * main - Entry point of the program
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: Always 0 (Success)
+ * Return: 0 on success, 1 on error
  */
-int main(void)
+int main(int ac, char **av)
 {
-	print_hello();
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	return (0);
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
+
